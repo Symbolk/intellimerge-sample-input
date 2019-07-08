4d540f28526aa2085d44e1aa493b70fe28a29037
Symbolk <symbolk@163.com>
2019-07-08 15:19:44 +0800

    [refactor] Rename director to mainDirector, since one movie could have more than one directors.

--- src/main/java/bad/robot/refactoring/chapter1/ComedyMovie.java
+++ src/main/java/bad/robot/refactoring/chapter1/ComedyMovie.java
@@ -1,17 +1,17 @@
 package bad.robot.refactoring.chapter1;
 
 public class ComedyMovie extends Movie {
-    private String director;
+    private String mainDirector;
 
     public ComedyMovie(String title, int priceCode) {
         super(title, priceCode);
     }
 
     public String getDirector() {
-        return director;
+        return mainDirector;
     }
 
     public void setDirector(String director) {
-        this.director = director;
+        this.mainDirector = director;
     }
 }
--- src/main/java/bad/robot/refactoring/chapter1/HorrorMovie.java
+++ src/main/java/bad/robot/refactoring/chapter1/HorrorMovie.java
@@ -1,16 +1,16 @@
 package bad.robot.refactoring.chapter1;
 
 public class HorrorMovie extends Movie{
-    private String director;
+    private String mainDirector;
     public HorrorMovie(String title, int priceCode) {
         super(title, priceCode);
     }
 
     public String getDirector() {
-        return director;
+        return mainDirector;
     }
 
     public void setDirector(String director) {
-        this.director = director;
+        this.mainDirector = director;
     }
 }
